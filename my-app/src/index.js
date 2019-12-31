5aa1c1c3be5ff77d4d007a90e32c7a5931966496
fcruzlopes <flopescontas@gmail.com>
2019-12-31 13:47:06 +0000

    Removed the site

--- my-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import Root from './root';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '/home/flops/Documents/temp/my-app/src/App.css';
-
-ReactDOM.render(Root(), document.getElementById('root'));
